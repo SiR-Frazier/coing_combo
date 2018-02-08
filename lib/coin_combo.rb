fbda4c9fab0b7419f60c94497f67a5ede0e48bba
Bryce Frazier and Chase Bowden <chase@>
2018-02-08 09:57:39 -0800

    3 working tests

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,15 +1,19 @@
 class Change
   def initialize
     @quarters = 0
+    @nickels = 0
   end
 
   def coins_back(input)
-    until input == 0
+    until input <= 0
       if input >= 25
         @quarters += 1
         input = input - 25
+       elsif input >= 5
+        @nickels =+ 1
+        input = input - 5
       end
     end
-    "#{@quarters} Quarters"
+    "#{@quarters} Quarters, #{@nickels} Nickels"
   end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,11 +5,14 @@ require('pry')
 describe("#coin_combo") do
   it("gives back a quarter if 25 cents is the change") do
     change = Change.new()
-    expect(change.coins_back(25)).to(eq("1 Quarters"))
+    expect(change.coins_back(25)).to(eq("1 Quarters, 0 Nickels"))
   end
   it("gives back two quarters if 50 cents is the change") do
     change = Change.new()
-    expect(change.coins_back(50)).to(eq("2 Quarters"))
+    expect(change.coins_back(50)).to(eq("2 Quarters, 0 Nickels"))
   end
-  
+  it("gives back one quarters and a nickel if 30 cents is the change") do
+    change = Change.new()
+    expect(change.coins_back(30)).to(eq("1 Quarters, 1 Nickels"))
+  end
 end
